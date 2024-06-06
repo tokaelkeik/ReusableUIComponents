3a06719e45826cd4539a6b9f7fe3a30829628117
Toka Elkeik <telkeik@identity-solutions.org>
2024-06-06 13:07:01 +0300

    Initial Commit

---
+++ GenericViews/GenericViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericViewsApp.swift
+//  GenericViews
+//
+//  Created by Toka Elkeik on 06/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct GenericViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
